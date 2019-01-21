6937ede7413c12414621a2f3ef8b715428af120f
Aleksandar Glišović <aleksandar@pine.rs>
2019-01-21 06:18:40 +0100

    Updates models and migrations

--- social_network/models.py
+++ social_network/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.email
+
+
 class Post(models.Model):
-    content = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
     date = models.DateTimeField('post date')
 
     def __str__(self):
@@ -9,5 +19,9 @@ class Post(models.Model):
 
 
 class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    date = models.DateTimeField('post date')
+    date = models.DateTimeField('like date')
+
+    def __str__(self):
+        return self.user.email
