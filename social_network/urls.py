6c1db3d0ec46eeeca29513b202dc3567f644e440
Aleksandar Glišović <aleksandar@pine.rs>
2019-01-21 07:03:31 +0100

    Creates initial urls and views

--- social_network/urls.py
+++ social_network/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.users, name='users'),
+    path('login/', views.users_login, name='users_login'),
+    path('signup/', views.users_signup, name='users_signup'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/create/', views.posts_create, name='posts_create'),
+    path('posts/like/<int:post_id>/', views.posts_like, name='posts_like'),
+    path('posts/unlike/<int:post_id>/', views.posts_unlike, name='posts_unlike'),
 ]
--- social_network/views.py
+++ social_network/views.py
@@ -1,4 +1,36 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the social network index.")
+
+def users(request):
+    users = "Users"
+    return HttpResponse(users)
+
+
+def users_signup(request):
+    users = "Users Signup"
+    return HttpResponse(users)
+
+
+def users_login(request):
+    users = "Users Login"
+    return HttpResponse(users)
+
+
+def posts(request):
+    posts = "Posts"
+    return HttpResponse(posts)
+
+
+def posts_create(request):
+    posts = "Posts Create"
+    return HttpResponse(posts)
+
+
+def posts_like(request, post_id):
+    posts = "Posts Like %s" % post_id
+    return HttpResponse(posts)
+
+
+def posts_unlike(request, post_id):
+    posts = "Posts Unlike %s" % post_id
+    return HttpResponse(posts)
