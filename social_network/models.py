8e436beb1ddec76c520716da3b43f5924a9e40cf
Aleksandar Glišović <aleksandar@pine.rs>
2019-01-23 03:46:20 +0100

    Adds last login to User model

--- social_network/models.py
+++ social_network/models.py
@@ -1,15 +1,24 @@
 from django.db import models
-
+from django.utils import timezone
 
 class User(models.Model):
+    """
+    User Model
+
+    """
     email = models.EmailField()
     password = models.CharField(max_length=200)
+    last_login = models.DateTimeField('last login', default=timezone.now)
 
     def __str__(self):
         return self.email
 
 
 class Post(models.Model):
+    """
+    Post Model
+
+    """
     user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField('post date')
@@ -19,6 +28,10 @@ class Post(models.Model):
 
 
 class Like(models.Model):
+    """
+    Like Model
+
+    """
     user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
     date = models.DateTimeField('like date')
