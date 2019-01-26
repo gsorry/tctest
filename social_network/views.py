d5566ddb80a418b84edddb16f4c1db1dc1416dc8
Aleksandar Glišović <aleksandar@pine.rs>
2019-01-26 08:33:19 +0100

    Adds email hunter support

--- social_network/views.py
+++ social_network/views.py
@@ -1,10 +1,13 @@
 from django.utils import timezone
 from django.contrib.auth import hashers
+from django.conf import settings
 
 from rest_framework import status
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+from pyhunter import PyHunter
 
 from .serializers import UserSerializer, PostSerializer, LikeSerializer
 
@@ -23,8 +26,11 @@ class UserViewSet(ViewSet):
 
     @action(detail=False, methods=['post'], name='User Signup')
     def signup(self, request):
+        hunter = PyHunter(settings.HUNTER_API_KEY)
         if (User.objects.filter(email=request.data['email']).exists()):
             return Response({"detail": "Email already taken."}, status=status.HTTP_400_BAD_REQUEST)
+        elif hunter.email_verifier(request.data['email']):
+            return Response({"detail": "Email not valid."}, status=status.HTTP_400_BAD_REQUEST)
         else:
             user = User(email=request.data['email'],
                         password=hashers.make_password(request.data['password']))
